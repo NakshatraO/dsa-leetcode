a335b3bcf39efcdebd65a390b1fbfbd6aa88193a
Nakshatra <nakshatraozarkar@gmail.com>
2026-07-06 23:23:15 +0530

    optimize 0509 - Fibonacci Number from O(2ⁿ) to O(n)

--- 0509-FibonacciNumber.java
+++ 0509-FibonacciNumber.java
@@ -1,14 +1,22 @@
 /*
 Problem: 509. Fibonacci Number
 
-Time Complexity: O(2ⁿ)
-Space Complexity: O(n)
+Time Complexity: O(n)
+Space Complexity: O(1)
 */
 
 class Solution {
     public int fib(int n) {
-        if(n < 0 || n > 30) return 0;
-        if(n == 1 || n == 0) return n;
-        return fib(n-1) + fib(n-2);
+        if (n <= 1)
+            return n;
+        int f1 = 0;
+        int f2 = 1;
+        int fn = 0;
+        for(int i=1 ; i<n ; i++) {
+            fn = f1 + f2;
+            f1 = f2;
+            f2 = fn;
+        }
+        return fn;
     }
 }
