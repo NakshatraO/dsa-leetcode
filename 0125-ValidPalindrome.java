4f576fceb5ae859608cd20e9bd1e6ec56a51609b
Nakshatra <nakshatraozarkar@gmail.com>
2026-07-07 23:06:12 +0530

    optimize 0125 - Valid Palindrome using Two Pointers

--- 0125-ValidPalindrome.java
+++ 0125-ValidPalindrome.java
@@ -2,20 +2,31 @@
 Problem: 125. Valid Palindrome
 
 Time Complexity: O(n)
-Space Complexity: O(n)
+Space Complexity: O(1)
 */
 
 class Solution {
     public boolean isPalindrome(String s) {
         String str = s.toLowerCase();
-        String str2 = "";
 
-        for(int i=0 ; i<str.length() ; i++){
-            if(Character.isLetterOrDigit(str.charAt(i)))
-                str2 += str.charAt(i); 
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left < right){
+            if(!((str.charAt(left)>='A' && str.charAt(left)<='Z') || (str.charAt(left)>='a' && str.charAt(left)<='z') || (str.charAt(left)>='0' && str.charAt(left)<='9')))
+                left++;
+
+            else if(!((str.charAt(right)>='A' && str.charAt(right)<='Z') || (str.charAt(right)>='a' && str.charAt(right)<='z') || (str.charAt(right)>='0' && str.charAt(right)<='9')))
+                right--;
+                
+            else{
+                if(str.charAt(left) != str.charAt(right))
+                return false;
+                left++;
+                right--;
+            }
+            
         }
-        
-        String rev = new StringBuilder(str2).reverse().toString();
-        return str2.equals(rev);
+        return true;
     }
 }
